b2944c3eda34e529783edde7f69cdfe7fce27761
yuanm <yuanm@DESKTOP-H9U2I3I>
2018-09-23 14:45:54 +0900

    ddl er図 修正

--- db/migration/DDL/V1__RESTANRANT.sql
+++ db/migration/DDL/V1__RESTANRANT.sql
@@ -1,174 +1,157 @@
 -- Project Name : ChineseGourmet
--- Date/Time    : 2018/09/23 13:21:29
+-- Date/Time    : 2018/09/23 14:37:07
 -- Author       : yuanm
--- RDBMS Type   : Oracle Database
+-- RDBMS Type   : MySQL
 -- Application  : A5:SQL Mk-2
 
 -- 権限
 create table authority (
-  authority VARCHAR2(200) not null
-  , role_id VARCHAR2(1) not null
-  , authority_id bigint auto_increment not null
+  authority VARCHAR(200) not null comment '権限'
+  , role_id VARCHAR(1) not null comment 'ロールID'
+  , authority_id bigint auto_increment not null comment '権限ID'
+  , version_no bigint not null comment 'バージョン番号'
+  , created_at DATETIME(6) not null comment '登録日時'
+  , created_by VARCHAR(15) not null comment '登録者'
+  , updated_at DATETIME(6) not null comment '更新日時'
+  , updated_by VARCHAR(15) not null comment '更新者'
+  , is_actived INT(1) not null comment 'アクティブフラグ:0：無効、1：有効'
   , constraint authority_PKC primary key (authority_id)
-) ;
+) comment '権限:' ;
 
 -- ユーザー
 create table user (
-  user_id bigint auto_increment not null
-  , user_name VARCHAR2(20)
-  , email VARCHAR2(30)
-  , password VARCHAR2(100)
-  , role_id VARCHAR2(1)
+  user_id bigint auto_increment not null comment 'ユーザーID'
+  , user_name VARCHAR(20) comment 'ユーザー名'
+  , email VARCHAR(30) comment 'メールアドレス'
+  , password VARCHAR(100) comment 'パスワード'
+  , role_id VARCHAR(1) comment 'ロールID'
+  , version_no bigint not null comment 'バージョン番号'
+  , created_at DATETIME(6) not null comment '登録日時'
+  , created_by VARCHAR(15) not null comment '登録者'
+  , updated_at DATETIME(6) not null comment '更新日時'
+  , updated_by VARCHAR(15) not null comment '更新者'
+  , is_actived INT(1) not null comment 'アクティブフラグ:0：無効、1：有効'
   , constraint user_PKC primary key (user_id)
-) ;
+) comment 'ユーザー:' ;
 
 -- クーポン保持
 create table coupon_hold (
-  coupon_hold_id bigint auto_increment not null
-  , coupon_id bigint not null
-  , device_id VARCHAR2(50) not null
-  , coupon_get_datetime DATE
+  coupon_hold_id bigint auto_increment not null comment 'クーポン保持ID'
+  , coupon_id bigint not null comment 'クーポンID'
+  , device_id VARCHAR(50) not null comment 'デバイスID'
+  , coupon_get_datetime DATETIME comment 'クーポン取得日時'
+  , version_no bigint not null comment 'バージョン番号'
+  , created_at DATETIME(6) not null comment '登録日時'
+  , created_by VARCHAR(15) not null comment '登録者'
+  , updated_at DATETIME(6) not null comment '更新日時'
+  , updated_by VARCHAR(15) not null comment '更新者'
+  , is_actived INT(1) not null comment 'アクティブフラグ:0：無効、1：有効'
   , constraint coupon_hold_PKC primary key (coupon_hold_id)
-) ;
+) comment 'クーポン保持:' ;
 
 -- クーポン
 create table coupon (
-  coupon_id bigint auto_increment not null
-  , restaurant_id bigint not null
-  , title VARCHAR2(200)
-  , detail VARCHAR2(200)
-  , coupon_start_date DATE
-  , coupon_end_date DATE
-  , coupon_total_amount NUMBER(10)
-  , coupon_status CHAR(1)
+  coupon_id bigint auto_increment not null comment 'クーポンID'
+  , restaurant_id bigint not null comment '店舗ID'
+  , title VARCHAR(200) comment 'タイトル'
+  , detail VARCHAR(200) comment '詳細'
+  , coupon_start_date DATE comment '有効開始日'
+  , coupon_end_date DATE comment '有効終了日'
+  , coupon_total_amount INT comment 'クーポン枚数'
+  , coupon_status CHAR(1) comment 'クーポンステータス'
+  , version_no bigint not null comment 'バージョン番号'
+  , created_at DATETIME(6) not null comment '登録日時'
+  , created_by VARCHAR(15) not null comment '登録者'
+  , updated_at DATETIME(6) not null comment '更新日時'
+  , updated_by VARCHAR(15) not null comment '更新者'
+  , is_actived INT(1) not null comment 'アクティブフラグ:0：無効、1：有効'
   , constraint coupon_PKC primary key (coupon_id)
-) ;
+) comment 'クーポン:' ;
 
 -- 店舗
 create table restaurant (
-  restaurant_id bigint auto_increment not null
-  , restaurant_name VARCHAR2(20)
-  , restaurant_manager VARCHAR2(20)
-  , restaurant_phone VARCHAR2(15)
-  , restaurant_url VARCHAR2(100)
-  , restaurant_image_url VARCHAR2(100)
-  , latitude NUMBER
-  , longitude NUMBER
-  , restaurant_status CHAR(1)
-  , restauranat_address VARCHAR2(200)
-  , restaurant_memo VARCHAR2(200)
+  restaurant_id bigint auto_increment not null comment '店舗ID'
+  , restaurant_name VARCHAR(20) comment '店舗名'
+  , restaurant_manager VARCHAR(20) comment '店舗担当者'
+  , restaurant_phone VARCHAR(15) comment '店舗連絡先'
+  , restaurant_url VARCHAR(100) comment '店舗URL'
+  , restaurant_image_url VARCHAR(100) comment '店舗画像URL'
+  , latitude FLOAT comment '緯度'
+  , longitude FLOAT comment '経度'
+  , restaurant_status CHAR(1) comment '店舗ステータス'
+  , restauranat_address VARCHAR(200) comment '店舗アドレス'
+  , restaurant_memo VARCHAR(200) comment '店舗備考'
+  , version_no bigint not null comment 'バージョン番号'
+  , created_at DATETIME(6) not null comment '登録日時'
+  , created_by VARCHAR(15) not null comment '登録者'
+  , updated_at DATETIME(6) not null comment '更新日時'
+  , updated_by VARCHAR(15) not null comment '更新者'
+  , is_actived INT(1) not null comment 'アクティブフラグ:0：無効、1：有効'
   , constraint restaurant_PKC primary key (restaurant_id)
-) ;
+) comment '店舗:' ;
 
 -- フィードバック
 create table feedback (
-  feedback_id bigint auto_increment not null
-  , device_id VARCHAR2(50) not null
-  , type VARCHAR2(2)
-  , detail VARCHAR2(200)
-  , treatment_status CHAR(1)
-  , treatment_memo VARCHAR2(200)
+  feedback_id bigint auto_increment not null comment 'フィードバックID'
+  , device_id VARCHAR(50) not null comment 'デバイスID'
+  , type VARCHAR(2) comment 'タイプ'
+  , detail VARCHAR(200) comment '詳細'
+  , treatment_status CHAR(1) comment '処理ステータス'
+  , treatment_memo VARCHAR(200) comment '処理備考'
+  , version_no bigint not null comment 'バージョン番号'
+  , created_at DATETIME(6) not null comment '登録日時'
+  , created_by VARCHAR(15) not null comment '登録者'
+  , updated_at DATETIME(6) not null comment '更新日時'
+  , updated_by VARCHAR(15) not null comment '更新者'
+  , is_actived INT(1) not null comment 'アクティブフラグ:0：無効、1：有効'
   , constraint feedback_PKC primary key (feedback_id)
-) ;
+) comment 'フィードバック:' ;
 
 -- 抽選応募履歴
-create table "抽選応募履歴" (
-  lottery_subs_history_id bigint auto_increment not null
-  , lottery_event_id bigint
-  , device_id VARCHAR2(50) not null
-  , lottery_status CHAR(1)
-  , validity_flag CHAR(1)
-  , constraint "抽選応募履歴_PKC" primary key (lottery_subs_history_id)
-) ;
+create table `抽選応募履歴` (
+  lottery_subs_history_id bigint auto_increment not null comment '抽選応募履歴ID'
+  , lottery_event_id bigint comment '抽選イベントID'
+  , device_id VARCHAR(50) not null comment 'デバイスID'
+  , lottery_status CHAR(1) comment '抽選ステータス:１：有効０：無効'
+  , validity_flag CHAR(1) comment '有効フラグ:１：有効０：無効'
+  , version_no bigint not null comment 'バージョン番号'
+  , created_at DATETIME(6) not null comment '登録日時'
+  , created_by VARCHAR(15) not null comment '登録者'
+  , updated_at DATETIME(6) not null comment '更新日時'
+  , updated_by VARCHAR(15) not null comment '更新者'
+  , is_actived INT(1) not null comment 'アクティブフラグ:0：無効、1：有効'
+  , constraint `抽選応募履歴_PKC` primary key (lottery_subs_history_id)
+) comment '抽選応募履歴:' ;
 
 -- 抽選イベント
 create table lottery_event (
-  lottery_event_id bigint auto_increment not null
-  , lottery_detail VARCHAR2(200)
-  , lottery_title VARCHAR2(80)
-  , lottery_image VARCHAR2(2048)
-  , start_datetime DATE
-  , end_datetime DATE
-  , announcement_datetime DATE
-  , coupon_id bigint
+  lottery_event_id bigint auto_increment not null comment '抽選イベントID'
+  , lottery_detail VARCHAR(200) comment '抽選詳細'
+  , lottery_title VARCHAR(80) comment '抽選タイトル'
+  , lottery_image VARCHAR(2048) comment '抽選画像'
+  , start_datetime DATETIME comment '応募開始日時'
+  , end_datetime DATETIME comment '応募終了日時'
+  , announcement_datetime DATETIME comment '結果発表日時'
+  , coupon_id bigint comment 'クーポンID'
+  , version_no bigint not null comment 'バージョン番号'
+  , created_at DATETIME(6) not null comment '登録日時'
+  , created_by VARCHAR(15) not null comment '登録者'
+  , updated_at DATETIME(6) not null comment '更新日時'
+  , updated_by VARCHAR(15) not null comment '更新者'
+  , is_actived INT(1) not null comment 'アクティブフラグ:0：無効、1：有効'
   , constraint lottery_event_PKC primary key (lottery_event_id)
-) ;
+) comment '抽選イベント:' ;
 
 -- 顧客
 create table customer (
-  customer_id bigint auto_increment not null
-  , device_id VARCHAR2(50) not null
-  , nick_name VARCHAR2(20)
+  customer_id bigint auto_increment not null comment '顧客ID'
+  , device_id VARCHAR(50) not null comment 'デバイスID'
+  , nick_name VARCHAR(20) comment 'ニックネーム'
+  , version_no bigint not null comment 'バージョン番号'
+  , created_at DATETIME(6) not null comment '登録日時'
+  , created_by VARCHAR(15) not null comment '登録者'
+  , updated_at DATETIME(6) not null comment '更新日時'
+  , updated_by VARCHAR(15) not null comment '更新者'
+  , is_actived INT(1) not null comment 'アクティブフラグ:0：無効、1：有効'
   , constraint customer_PKC primary key (customer_id)
-) ;
-
-comment on table authority is '権限';
-comment on column authority.authority is '権限';
-comment on column authority.role_id is 'ロールID';
-comment on column authority.authority_id is '権限ID';
-
-comment on table user is 'ユーザー';
-comment on column user.user_id is 'ユーザーID';
-comment on column user.user_name is 'ユーザー名';
-comment on column user.email is 'メールアドレス';
-comment on column user.password is 'パスワード';
-comment on column user.role_id is 'ロールID';
-
-comment on table coupon_hold is 'クーポン保持';
-comment on column coupon_hold.coupon_hold_id is 'クーポン保持ID';
-comment on column coupon_hold.coupon_id is 'クーポンID';
-comment on column coupon_hold.device_id is 'デバイスID';
-comment on column coupon_hold.coupon_get_datetime is 'クーポン取得日時';
-
-comment on table coupon is 'クーポン';
-comment on column coupon.coupon_id is 'クーポンID';
-comment on column coupon.restaurant_id is '店舗ID';
-comment on column coupon.title is 'タイトル';
-comment on column coupon.detail is '詳細';
-comment on column coupon.coupon_start_date is '有効開始日';
-comment on column coupon.coupon_end_date is '有効終了日';
-comment on column coupon.coupon_total_amount is 'クーポン枚数';
-comment on column coupon.coupon_status is 'クーポンステータス';
-
-comment on table restaurant is '店舗';
-comment on column restaurant.restaurant_id is '店舗ID';
-comment on column restaurant.restaurant_name is '店舗名';
-comment on column restaurant.restaurant_manager is '店舗担当者';
-comment on column restaurant.restaurant_phone is '店舗連絡先';
-comment on column restaurant.restaurant_url is '店舗URL';
-comment on column restaurant.restaurant_image_url is '店舗画像URL';
-comment on column restaurant.latitude is '緯度';
-comment on column restaurant.longitude is '経度';
-comment on column restaurant.restaurant_status is '店舗ステータス';
-comment on column restaurant.restauranat_address is '店舗アドレス';
-comment on column restaurant.restaurant_memo is '店舗備考';
-
-comment on table feedback is 'フィードバック';
-comment on column feedback.feedback_id is 'フィードバックID';
-comment on column feedback.device_id is 'デバイスID';
-comment on column feedback.type is 'タイプ';
-comment on column feedback.detail is '詳細';
-comment on column feedback.treatment_status is '処理ステータス';
-comment on column feedback.treatment_memo is '処理備考';
-
-comment on table "抽選応募履歴" is '抽選応募履歴';
-comment on column "抽選応募履歴".lottery_subs_history_id is '抽選応募履歴ID';
-comment on column "抽選応募履歴".lottery_event_id is '抽選イベントID';
-comment on column "抽選応募履歴".device_id is 'デバイスID';
-comment on column "抽選応募履歴".lottery_status is '抽選ステータス:１：有効０：無効';
-comment on column "抽選応募履歴".validity_flag is '有効フラグ:１：有効０：無効';
-
-comment on table lottery_event is '抽選イベント:';
-comment on column lottery_event.lottery_event_id is '抽選イベントID';
-comment on column lottery_event.lottery_detail is '抽選詳細';
-comment on column lottery_event.lottery_title is '抽選タイトル';
-comment on column lottery_event.lottery_image is '抽選画像';
-comment on column lottery_event.start_datetime is '応募開始日時';
-comment on column lottery_event.end_datetime is '応募終了日時';
-comment on column lottery_event.announcement_datetime is '結果発表日時';
-comment on column lottery_event.coupon_id is 'クーポンID';
-
-comment on table customer is '顧客';
-comment on column customer.customer_id is '顧客ID';
-comment on column customer.device_id is 'デバイスID';
-comment on column customer.nick_name is 'ニックネーム';
+) comment '顧客:' ;

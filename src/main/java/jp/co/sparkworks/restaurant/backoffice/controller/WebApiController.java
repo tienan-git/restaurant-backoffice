1c080a09a4ac3970ab40ea4a45b9c108a257f30c
ソウ リチョウ <soh.richoh@sparkworks.co.jp>
2018-09-24 00:10:48 +0900

    fix

--- src/main/java/jp/co/sparkworks/restaurant/backoffice/controller/WebApiController.java
+++ src/main/java/jp/co/sparkworks/restaurant/backoffice/controller/WebApiController.java
@@ -26,147 +26,101 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class WebApiController {
 
-	@Autowired
-	WebAPIService webAPIService;
+    @Autowired
+    WebAPIService webAPIService;
 
-	// １、同期
-	// POST /synchronizations/{deviceId}
-	@PostMapping("/synchronization/{deviceId}")
-	public SynchronizationRes postSynchronization(@PathVariable String deviceId, @RequestBody SynchronizationReq req) {
+    // １、同期
+    // POST /synchronizations/{deviceId}
+    @PostMapping("/synchronization/{deviceId}")
+    public SynchronizationRes postSynchronization(@PathVariable String deviceId, @RequestBody SynchronizationReq req) {
 
-		List<CouponDto> couponDtoList = webAPIService.synchronization(deviceId, req.getNickName());
+        List<CouponDto> couponDtoList = webAPIService.synchronization(deviceId, req.getNickName());
 
-		SynchronizationRes res = new SynchronizationRes();
-		res.setCode(ResultCodeConstants.I000);
-		res.setMessage("取得しました");
-		res.setCouponDtoList(couponDtoList);
-		return res;
+        SynchronizationRes res = new SynchronizationRes();
+        res.setCode(ResultCodeConstants.I000);
+        res.setMessage("取得しました");
+        res.setCouponDtoList(couponDtoList);
+        return res;
 
-	}
+    }
 
-	// ２、店一覧
-	// GET /restaurants/{deviceId}
-	@GetMapping("/restaurants/{deviceId}")
-	public GetRestaurantsDeviceIdRes getRestaurantsDeviceId(@PathVariable String deviceId) {
+    // ２、店一覧
+    // GET /restaurants/{deviceId}
+    @GetMapping("/restaurants/{deviceId}")
+    public GetRestaurantsDeviceIdRes getRestaurantsDeviceId(@PathVariable String deviceId) {
 
-		List<RestaurantDto> restaurantDtoList = webAPIService.getRestaurants();
+        List<RestaurantDto> restaurantDtoList = webAPIService.getRestaurants();
 
-		GetRestaurantsDeviceIdRes res = new GetRestaurantsDeviceIdRes();
-		res.setCode(ResultCodeConstants.I000);
-		res.setMessage("取得しました");
-		res.setRestaurantDtoList(restaurantDtoList);
-		return res;
+        GetRestaurantsDeviceIdRes res = new GetRestaurantsDeviceIdRes();
+        res.setCode(ResultCodeConstants.I000);
+        res.setMessage("取得しました");
+        res.setRestaurantDtoList(restaurantDtoList);
+        return res;
 
-	}
+    }
 
-	// ３、クーポン追加
-	// POST /coupons/{deviceId}/{couponId}
-	@PostMapping("/coupons/{deviceId}/{couponId}")
-	public BaseRes postCoupons(@PathVariable String deviceId, @PathVariable String couponId) {
+    // ３、クーポン追加
+    // POST /coupons/{deviceId}/{couponId}
+    @PostMapping("/coupons/{deviceId}/{couponId}")
+    public BaseRes postCoupons(@PathVariable String deviceId, @PathVariable String couponId) {
 
-		webAPIService.postCoupons(deviceId, couponId);
+        webAPIService.postCoupons(deviceId, couponId);
 
-		return BaseRes.SUCCESS;
+        return BaseRes.SUCCESS;
 
-	}
-	// ４、クーポン削除
-	// DELETE /coupons/{deviceId}/{couponId}
+    }
+    // ４、クーポン削除
+    // DELETE /coupons/{deviceId}/{couponId}
 
-	@DeleteMapping("/coupons/{deviceId}/{couponId}")
-	public BaseRes deleteCoupons(@PathVariable String deviceId, @PathVariable String couponId) {
+    @DeleteMapping("/coupons/{deviceId}/{couponId}")
+    public BaseRes deleteCoupons(@PathVariable String deviceId, @PathVariable String couponId) {
 
-		webAPIService.postCoupons(deviceId, couponId);
+        webAPIService.postCoupons(deviceId, couponId);
 
-		return BaseRes.SUCCESS;
+        return BaseRes.SUCCESS;
 
-	}
+    }
 
-<<<<<<< HEAD
-	// ５、今の抽選
-	// GET /lotteries/{deviceId}
-	@GetMapping("/lotteries/{deviceId}")
-	public BaseRes getCoupons(@PathVariable String deviceId) {
-=======
     // ５、今の抽選
     // GET /lotteries/{deviceId}
-    @GetMapping("/coupons/{deviceId}/{couponId}")
-    public BaseRes getCoupons(@PathVariable String deviceId, @PathVariable String couponId) {
->>>>>>> branch 'develop' of https://gitlab.com/sparkworks/Restaurant/restaurant-backoffice.git
+    @GetMapping("/lotteries/{deviceId}")
+    public BaseRes getCoupons(@PathVariable String deviceId) {
 
-		webAPIService.getCoupons();
+        webAPIService.getCoupons();
 
-		return BaseRes.SUCCESS;
-	}
-
-<<<<<<< HEAD
-	// ６、抽選応募
-	// POST /lotteries/{lotteryId}/{deviceId}
-	@PostMapping("/lotteries/{lotteryId}/{deviceId}")
-	public BaseRes postCouponsDeviceId(@PathVariable String deviceId, @PathVariable String lotteryId) {
-=======
-    // ６、抽選応募
-    // POST /lotteries/{lotteryId}/{deviceId}
-    @PostMapping("/coupons/{deviceId}/{couponId}")
-    public BaseRes postCouponsDeviceId(@PathVariable String deviceId, @PathVariable String couponId) {
->>>>>>> branch 'develop' of https://gitlab.com/sparkworks/Restaurant/restaurant-backoffice.git
-
-<<<<<<< HEAD
-		webAPIService.postCouponsDeviceId(deviceId, lotteryId);
-=======
-        webAPIService.postCouponsDeviceId(deviceId,couponId);
->>>>>>> branch 'develop' of https://gitlab.com/sparkworks/Restaurant/restaurant-backoffice.git
-
-<<<<<<< HEAD
-		return BaseRes.SUCCESS;
-	}
-
-	// ７、抽選履歴取得
-	// GET /lotteries/histories/{deviceId}
-	@GetMapping("/lotteries/histories/{deviceId}")
-	public BaseRes getCouponsHistories(@PathVariable String deviceId) {
-=======
         return BaseRes.SUCCESS;
     }
+
+    // ６、抽選応募
+    // POST /lotteries/{lotteryId}/{deviceId}
+    @PostMapping("/lotteries/{lotteryId}/{deviceId}")
+    public BaseRes postCouponsDeviceId(@PathVariable String deviceId, @PathVariable String lotteryId) {
+        webAPIService.postCouponsDeviceId(deviceId, lotteryId);
+        return BaseRes.SUCCESS;
+    }
+
     // ７、抽選履歴取得
     // GET /lotteries/histories/{deviceId}
     @GetMapping("/lotteries/histories/{deviceId}")
     public BaseRes getCouponsHistories(@PathVariable String deviceId) {
->>>>>>> branch 'develop' of https://gitlab.com/sparkworks/Restaurant/restaurant-backoffice.git
 
-		webAPIService.getCouponsHistories(deviceId);
-
-<<<<<<< HEAD
-		return BaseRes.SUCCESS;
-	}
-
-	// ８、フィードバック
-	// POST /feedbacks/{deviceId}
-	@PostMapping("/feedbacks/{deviceId}")
-	public BaseRes postFeedbacks(@PathVariable String deviceId, @RequestBody PostFeedbacksReq req) {
-=======
+        webAPIService.getCouponsHistories(deviceId);
         return BaseRes.SUCCESS;
     }
-    
-    
+
     // ８、フィードバック
     // POST /feedbacks/{deviceId}
-    @PostMapping("/synchronization/{deviceId}")
-    public SynchronizationRes postFeedbacks(@PathVariable String deviceId, @RequestBody SynchronizationReq req) {
->>>>>>> branch 'develop' of https://gitlab.com/sparkworks/Restaurant/restaurant-backoffice.git
+    @PostMapping("/feedbacks/{deviceId}")
+    public BaseRes postFeedbacks(@PathVariable String deviceId, @RequestBody PostFeedbacksReq req) {
 
-		FeedbackDto feedbackDto = new FeedbackDto();
-		feedbackDto.setType(req.getType());
-		feedbackDto.setDetail(req.getDetail());
+        FeedbackDto feedbackDto = new FeedbackDto();
+        feedbackDto.setType(req.getType());
+        feedbackDto.setDetail(req.getDetail());
 
-		webAPIService.postFeedbacks(deviceId, feedbackDto);
+        webAPIService.postFeedbacks(deviceId, feedbackDto);
 
-<<<<<<< HEAD
-		return BaseRes.SUCCESS;
+        return BaseRes.SUCCESS;
 
-	}
-
-=======
     }
-    
->>>>>>> branch 'develop' of https://gitlab.com/sparkworks/Restaurant/restaurant-backoffice.git
+
 }

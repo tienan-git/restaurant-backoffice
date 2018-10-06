7330f3cb75b922ac2ae03075e8614a58d73a3ad9
luoq1 <luoq1@LAPTOP-PPNEBOC0>
2018-10-06 19:02:03 +0900

    抽選backoffice修正

--- src/main/java/jp/co/sparkworks/restaurant/backoffice/dto/LotteryDto.java
+++ src/main/java/jp/co/sparkworks/restaurant/backoffice/dto/LotteryDto.java
@@ -2,6 +2,7 @@ package jp.co.sparkworks.restaurant.backoffice.dto;
 
 import java.time.LocalDateTime;
 
+import jp.co.sparkworks.restaurant.backoffice.util.DateUtil;
 import lombok.Data;
 
 @Data
@@ -15,4 +16,26 @@ public class LotteryDto {
 	LocalDateTime announcementDatetime;
 	Long couponId;
 	int count;
+	
+	public String getStartDatetimeFormat() {
+		if(startDatetime == null) {
+			return null;
+		}
+		return DateUtil.localDateTimeFormat(startDatetime);	
+	}
+	
+	public String getEndDatetimeFormat() {
+		if(endDatetime == null) {
+			return null;
+		}
+		return DateUtil.localDateTimeFormat(endDatetime);	
+	}
+	
+	public String getAnnouncementDatetimeFormat() {
+		if(announcementDatetime == null) {
+			return null;
+		}
+		return DateUtil.localDateTimeFormat(announcementDatetime);	
+	}
+	
 }

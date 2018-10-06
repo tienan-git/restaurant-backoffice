7ca4ecaad96e39919814d40a064e51846c207306
金政翰 <kin.seikan@sparkworks.co.jp>
2018-10-06 17:39:16 +0900

    feedback

--- src/main/java/jp/co/sparkworks/restaurant/backoffice/service/FeedbackService.java
+++ src/main/java/jp/co/sparkworks/restaurant/backoffice/service/FeedbackService.java
@@ -2,8 +2,6 @@ package jp.co.sparkworks.restaurant.backoffice.service;
 
 import java.util.List;
 
-import jp.co.sparkworks.restaurant.backoffice.dto.CustomerDto;
-import jp.co.sparkworks.restaurant.backoffice.dto.CustomerSearchDto;
 import jp.co.sparkworks.restaurant.backoffice.dto.FeedbackDto;
 import jp.co.sparkworks.restaurant.backoffice.dto.FeedbackSearchDto;
 
@@ -11,5 +9,7 @@ public interface FeedbackService {
 
 	
 	List<FeedbackDto> searchAll(FeedbackSearchDto feedbackSearchDto);
-
+	void update(FeedbackDto feedbackDto);
+	
+	FeedbackDto searchById(Long feedbackId);
 }
